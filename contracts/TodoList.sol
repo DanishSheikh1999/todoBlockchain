a60cba2a04f77e8cac091cfb8c0b79e8299a6add
Danish Sheikh <danishindia1999@gmial.com>
2021-05-25 23:47:09 +0530

    Etherium Smart Contract

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,6 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract TodoList{
-    uint public taskCount = 0;
+contract TodoList {
+    uint256 public taskCount = 0;
+
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint256 => Task) public tasks;
+
+    constructor() public {
+        createTask("Hello Blockchain duniya");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
